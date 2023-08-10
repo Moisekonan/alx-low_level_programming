bfd8aaf858d852e78af66d3637fdbb2e26a4c863
Moisekonan <mokonan99@gmail.com>
2023-08-10 11:36:00 -0700

    gg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific sentence using the puts
+ * function and returns 0 to indicate success.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific sentence using printf
+ *              function and returns 0 to indicate success.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
