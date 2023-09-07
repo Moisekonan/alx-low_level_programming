af9f03a9fe6058ec7fe2f3afd33d07cfc8e7a541
Moisekonan <mokonan99@gmail.com>
2023-09-07 03:50:10 -0700

    update 1- et 101

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,52 +1,45 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: seconds string
- * @n: s2 char number to be used
- * Return: pointer or 98
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr = NULL;
-	unsigned int length1 = 0, length2 = 0, tmp_length = 0, i = 0, j = 0;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[length1] != '\0')
-	{
-		length1++;
-	}
-	while (s1[length2] != '\0')
-	{
-		length2++;
-	}
-	if (n >= length2)
-		tmp_length = length2;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		tmp_length = length2 - n;
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	ptr = malloc(sizeof(char) * (tmp_length + length1 + 1));
-	if (ptr == NULL)
+	if (!s)
 		return (NULL);
 
-	while (i < length1)
+	while (i < len1)
 	{
-		ptr[i] = s1[i];
+		s[i] = s1[i];
 		i++;
 	}
 
-	while (j < tmp_length)
-	{
-		ptr[i] = s2[j];
-		i++;
-		j++;
-	}
-	ptr[i] = '\0';
-	return (ptr);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
